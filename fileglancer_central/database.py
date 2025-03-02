ecf781a52897f073c1804898eb18bab733dad5b4
Konrad Rokicki <krokicki@gmail.com>
2025-03-01 23:16:19 -0500

    refactor database/wiki code and add settings

---
+++ fileglancer_central/database.py
@@ -0,0 +1,83 @@
+
+from fileglancer_central.settings import get_settings
+from loguru import logger
+settings = get_settings()
+
+from sqlalchemy import create_engine, Column, String, Integer
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+Base = declarative_base()
+
+class FileSharePathDB(Base):
+    __tablename__ = 'file_share_paths'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    lab = Column(String)
+    storage = Column(String)
+    mac_path = Column(String)
+    smb_path = Column(String)
+    linux_path = Column(String, index=True, unique=True)
+    ad_group = Column(String, index=True)
+
+
+def get_db_session():
+    """Create and return a database session"""
+    engine = create_engine(settings.db_url)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    Base.metadata.create_all(engine)
+    return session
+
+
+def get_all_paths(session):
+    """Get all file share paths from the database"""
+    return session.query(FileSharePathDB).all()
+
+
+def update_database(session, table):
+    """Update database with new table data"""
+    # Get all existing linux_paths from database
+    existing_paths = {path[0] for path in session.query(FileSharePathDB.linux_path).all()}
+    new_paths = set()
+    num_existing = 0
+    num_new = 0
+
+    # Update or insert records
+    for _, row in table.iterrows():
+        linux_path = row[table.columns[4]]
+        new_paths.add(linux_path)
+        
+        # Check if path exists
+        existing_record = session.query(FileSharePathDB).filter_by(linux_path=linux_path).first()
+        
+
+        if existing_record:
+            # Update existing record
+            existing_record.lab = row[table.columns[0]]
+            existing_record.storage = row[table.columns[1]]
+            existing_record.mac_path = row[table.columns[2]]
+            existing_record.smb_path = row[table.columns[3]]
+            existing_record.ad_group = row[table.columns[5]]
+            num_existing += 1
+
+        else:
+            # Create new record
+            new_record = FileSharePathDB(
+                lab=row[table.columns[0]],
+                storage=row[table.columns[1]],
+                mac_path=row[table.columns[2]],
+                smb_path=row[table.columns[3]],
+                linux_path=linux_path,
+                ad_group=row[table.columns[5]]
+            )
+            session.add(new_record)
+            num_new += 1
+
+    logger.debug(f"Updated {num_existing} file share paths, added {num_new} file share paths")
+
+    # Delete records that no longer exist in the wiki
+    paths_to_delete = existing_paths - new_paths
+    if paths_to_delete:
+        logger.debug(f"Deleting {len(paths_to_delete)} defunct file share paths from the database")
+        session.query(FileSharePathDB).filter(FileSharePathDB.linux_path.in_(paths_to_delete)).delete(synchronize_session='fetch')
+
+    session.commit()
--- fileglancer_central/wiki.py
+++ fileglancer_central/wiki.py
@@ -10,42 +10,25 @@ https://wikis.janelia.org/plugins/personalaccesstokens/usertokens.action
 import os
 from io import StringIO
 import pandas as pd
-import pydantic
 from datetime import datetime
 from atlassian import Confluence
-from sqlalchemy import create_engine, Column, String, Integer
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from .database import get_db_session, update_database
+from .settings import get_settings
+from loguru import logger
+settings = get_settings()
 
 
-if __name__ == "__main__":  
-
-    # SQLAlchemy setup
-    Base = declarative_base()
-    engine = create_engine('sqlite:///fileglancer.db')
-    Session = sessionmaker(bind=engine)
-    session = Session()
-
-    class FileSharePathDB(Base):
-        __tablename__ = 'file_share_paths'
-        
-        id = Column(Integer, primary_key=True, autoincrement=True)
-        lab = Column(String)
-        storage = Column(String)
-        mac_path = Column(String)
-        smb_path = Column(String)
-        linux_path = Column(String, index=True, unique=True)
-        ad_group = Column(String, index=True)
-
-    Base.metadata.create_all(engine)
-
-    def parse_iso_timestamp(timestamp):
-        return datetime.fromisoformat(timestamp)
+def parse_iso_timestamp(timestamp):
+    """Parse ISO format timestamp string to datetime object"""
+    return datetime.fromisoformat(timestamp)
 
 
-    confluence_url = "https://wikis.janelia.org"
-    confluence_pat = os.environ.get('CONFLUENCE_TOKEN')
-    confluence = Confluence(url=confluence_url, token=confluence_pat)
+def get_wiki_table():
+    """Fetch and parse the file share paths table from the wiki"""
+    confluence_url = settings.confluence_url
+    confluence_pat = settings.confluence_token
+    confluence = Confluence(url=str(confluence_url), token=confluence_pat)
+    
     page = confluence.get_page_by_title("SCS", "Lab and Project File Share Paths")
     page_id = page['id']
     page = confluence.get_page_by_id(page_id, status=None, version=None,
@@ -57,8 +40,6 @@ if __name__ == "__main__":
         print(lastUpdated)
 
     body = page['body']['view']['value']
-    #print(body)
-
     tables = pd.read_html(StringIO(body))
     table = tables[0]
 
@@ -70,22 +51,12 @@ if __name__ == "__main__":
                 table.at[index, column] = last_valid_value
             else:
                 last_valid_value = value
+    
+    logger.debug(f"Found {len(table)} file share paths in the wiki")  
+    return table
 
 
-    for index, row in table.iterrows():
-        file_share_path_db = FileSharePathDB(
-            lab=table.columns[0],
-            storage=table.columns[1],
-            mac_path=table.columns[2],
-            smb_path=table.columns[3],
-            linux_path=table.columns[4],
-            ad_group=table.columns[5]
-        )
-        session.add(file_share_path_db)
-
-    session.commit()
-
-    #unique_values_column_1 = table[table.columns[1]].unique()
-    #print("Unique values for column 1:", unique_values_column_1)
-    #import json
-    #print(json.dumps(file_share_paths[0].model_dump(), indent=4))
+if __name__ == "__main__":
+    session = get_db_session()
+    table = get_wiki_table()
+    update_database(session, table)
